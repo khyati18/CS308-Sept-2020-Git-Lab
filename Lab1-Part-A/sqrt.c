82e9e6ee7a0bdb96b427b326b6cb1515219e544d
khyati18 <agarwal.khyati5@gmail.com>
2020-10-12 06:02:50 +0530

    Initial working code for sqrt

---
+++ Lab1-Part-A/sqrt.c
@@ -0,0 +1,22 @@
+/* This program computes the square root of a
+positive number. */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// To run code "./a.out number_whose_square_root_is_to_be_found"
+int main(int argc, char* argv[]) {
+	
+	if (argc != 2) 
+	{
+		printf("Usage: ./a.out sqrt_input\n");
+		exit(-1);
+	}
+
+	int input = atoi(argv[1]);
+	printf("Sqrt of %d is %f\n",input,sqrt(input));
+	return(0);
+
+} 
+// end main
