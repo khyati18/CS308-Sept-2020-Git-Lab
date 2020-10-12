e732ec94e690fbc1b2b3a6b5ffb094a5e8a69bf4
khyati18 <agarwal.khyati5@gmail.com>
2020-10-12 08:56:53 +0530

    Handling negative numbers

--- Lab2-Part-A/sqrt.c
+++ Lab2-Part-A/sqrt.c
@@ -6,6 +6,9 @@ positive number. */
 #include <math.h>
 #include <iostream>
 #include <cstring>
+#include <string>
+
+using namespace std;
 
 bool isNumber(char* s)
 {
@@ -16,6 +19,16 @@ bool isNumber(char* s)
     return true;
 }
 
+bool isNegative(char* s)
+{
+    if (s[0]=='-')
+    {
+    	return true;
+    }
+ 
+    return false;
+}
+
 // To run code "./a.out number_whose_square_root_is_to_be_found"
 int main(int argc, char* argv[]) {
 	
@@ -23,6 +36,19 @@ int main(int argc, char* argv[]) {
 	{
 		printf("Usage: ./a.out sqrt_input\n");
 		exit(-1);
+	}
+	
+	//If input is negative
+	if(isNegative(argv[1]))
+	{
+		string num_mod;
+		for (int i = 1; i < strlen(argv[1]); i++)
+        	{
+        		num_mod += argv[1][i];
+        	}
+        	int input = stoi(num_mod);
+        	printf("Sqrt of %d is %fi\n",input,sqrt(input));
+        	return 0;
 	}
 	
 	//If input is not an integer
