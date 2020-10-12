1bfbe1c6992117dc4333bd199a1c8e42f85a0559
khyati18 <agarwal.khyati5@gmail.com>
2020-10-12 08:09:49 +0530

    Changes for Validating the input

--- Lab2-Part-A/sqrt.c
+++ Lab2-Part-A/sqrt.c
@@ -4,6 +4,17 @@ positive number. */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <iostream>
+#include <cstring>
+
+bool isNumber(char* s)
+{
+    for (int i = 0; i < strlen(s); i++)
+        if (isdigit(s[i]) == false)
+            return false;
+ 
+    return true;
+}
 
 // To run code "./a.out number_whose_square_root_is_to_be_found"
 int main(int argc, char* argv[]) {
@@ -11,6 +22,13 @@ int main(int argc, char* argv[]) {
 	if (argc != 2) 
 	{
 		printf("Usage: ./a.out sqrt_input\n");
+		exit(-1);
+	}
+	
+	//If input is not an integer
+	if(!isNumber(argv[1]))
+	{
+		printf("Input should be an integer\n");
 		exit(-1);
 	}
 
