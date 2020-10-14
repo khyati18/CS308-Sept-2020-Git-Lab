6924c951d6dab9d4fff1c1c016fbdf8e3e697098
khyati18 <agarwal.khyati5@gmail.com>
2020-10-14 17:06:33 +0530

    Lab2-Part-B : Initial Commit main program and header file

---
+++ Lab2-Part-B/veclib.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define DIM 4
+// vector type
+typedef float vector[DIM];
+
+// functions
+
+void input(vector vec_a);
+// print vector
+void print(vector vec_a);
+// vector addition
+void add(vector vec_a, vector vec_b, vector vec_c);
+// elementwise product
+void eleProd(vector vec_a, vector vec_b, vector vec_c);
+// difference of two vectors
+void diff(vector vec_a, vector vec_b, vector vec_c);
+// dot product
+float dotProd(vector vec_a, vector vec_b);
---
+++ Lab2-Part-B/vecmain.c
@@ -0,0 +1,32 @@
+#include "veclib.h"
+
+void print(vector vec_a) {
+	printf("[");
+	for (int i=0;i<DIM;i++)
+		printf("%f ",vec_a[i]);
+	printf("]");
+	printf("\n");
+}
+
+
+void input(vector vec_a) {
+	for (int i=0;i<DIM;i++)
+		scanf("%f",&vec_a[i]);
+}
+
+
+int main() {
+	vector vecA, vecB, vecC, vecD;
+
+	// read vectors A and B
+	printf("Enter vector A with dimension %d\n", DIM);
+	input(vecA);
+	printf("Enter vector B with dimension %d\n", DIM);
+	input(vecB);
+	
+	printf("Vector A : ");
+	print(vecA);
+	printf("Vector B : ");
+	print(vecB);
+
+} // end main
